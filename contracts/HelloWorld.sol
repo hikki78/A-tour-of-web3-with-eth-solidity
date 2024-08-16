3466d7a23f897abb61594161bb95eca2b12bee03
hikki78 <meghadrimukherjee680@gmail.com>
2024-08-17 01:46:52 +0530

    step-1

---
+++ contracts/HelloWorld.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.0;
+
+contract HelloWorld {
+    function Hello() public pure returns (string memory) {
+        return "Hello, World";
+        
+    }
+}
